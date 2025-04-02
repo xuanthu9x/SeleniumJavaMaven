e0616e51682abe8f3301ee12eed0d210b1d1ee9e
xuanthu9x <1999xuanthu.com@gmail.com>
2025-04-02 19:35:08 +0700

    add source code
    add source code

---
+++ src/test/java/selenium/testSelenium.java
@@ -0,0 +1,14 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testSelenium {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://selenium.dev");
+
+        driver.quit();
+    }
+}
