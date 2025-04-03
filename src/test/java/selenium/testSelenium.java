94148d9e254d6547fdabe952fe950f1849928814
xuanthu9x <1999xuanthu.com@gmail.com>
2025-04-03 22:42:58 +0700

    Bai4_Locator_B1

--- src/test/java/selenium/testSelenium.java
+++ src/test/java/selenium/testSelenium.java
@@ -7,7 +7,19 @@ public class testSelenium {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://selenium.dev");
+        driver.get("https://crm.anhtester.com/admin/authentication");
+        driver.findElement(By.id("email")).sendKeys("admin@example.com");
+
+        driver.findElement(By.name("password")).sendKeys("123456");
+
+        // driver.findElement(By.linkText("Forgot Password?")).click();
+        // driver.findElement(By.partialLinkText("Forgot")).click();
+
+        //driver.findElement(By.tagName("button")).click();
+
+        driver.findElement(By.className("btn-primary")).click();
+
+        Thread.sleep(3000);
 
         driver.quit();
     }
