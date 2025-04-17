56ecbb4ff8c7232a709fab6bf823e21fce38942e
xuanthu9x <1999xuanthu.com@gmail.com>
2025-04-18 00:19:59 +0700

    get locator xpath tương đối, cách 1-> 5

--- src/test/java/selenium/GetLocatorByAttribute.java
+++ src/test/java/selenium/GetLocatorByAttribute.java
@@ -11,12 +11,15 @@ public class GetLocatorByAttribute {
 
 
         driver.get("https://crm.anhtester.com/admin/authentication");
+        //1 get locator by id
         driver.findElement(By.id("email")).sendKeys("admin@example.com");
+        // 2 get locator by name
         driver.findElement(By.name("password")).sendKeys("123456");
-
+        // 3. get locator by link text
         // driver.findElement(By.linkText("Forgot Password?")).click();
+        // 4. get locator by partialLinkText
         // driver.findElement(By.partialLinkText("Forgot")).click();
-
+        // 5. get locator by ClassName
         //driver.findElement(By.tagName("button")).click();
 
         driver.findElement(By.className("btn-primary")).click();
--- src/test/java/selenium/GetLocatorByXPath.java
+++ src/test/java/selenium/GetLocatorByXPath.java
@@ -2,9 +2,12 @@ package selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.lang.reflect.Array;
 import java.time.Duration;
+import java.util.List;
 
 public class GetLocatorByXPath {
     public WebDriver driver = new ChromeDriver();
@@ -14,14 +17,33 @@ public class GetLocatorByXPath {
         auto.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         auto.driver.get("https://crm.anhtester.com/admin/authentication");
         // Xpath tuyệt đối - login
-        auto.driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/form/div[1]/input")).sendKeys("admin@example.com");
-        auto.driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/form/div[2]/input")).sendKeys("123456");
-        auto.driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/form/div[4]/button")).click();
+            auto.driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/form/div[1]/input")).sendKeys("admin@example.com");
+            auto.driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/form/div[2]/input")).sendKeys("123456");
+            auto.driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[2]/form/div[4]/button")).click();
 
-        // Xpath tương đối - go to customer page, open a customer to view, view contact of the customer
-        auto.driver.findElement(By.xpath("//ul/li[contains(@class,'menu-item-customers')]")).click();
-        auto.driver.findElement(By.xpath("//tr/td[contains(@class,'sorting_1')]/a")).click();
-        auto.driver.findElement(By.xpath("//ul/li[contains(@class,'customer_tab_contacts')]/a")).click();
+        // Xpath tương đối 1 & 3 xác định bằng thuộc tính and sử dụng hàm contains()
+        // - go to customer page, open a customer to view, view contact of the customer
+            auto.driver.findElement(By.xpath("//ul/li[contains(@class,'menu-item-customers')]")).click();
+            auto.driver.findElement(By.xpath("//tr/td[contains(@class,'sorting_1')]/a")).click();
+            auto.driver.findElement(By.xpath("//ul/li[contains(@class,'customer_tab_profile')]/a")).click();
+        // Xpath tương đối 1. sử dụng hàm text(), normalize-spact()
+            auto.driver.findElement(By.xpath("//a[normalize-space() = 'Billing & Shipping']")).click();
+        // Xpath tương đối 4. Starts-with
+            List<WebElement> menuItems = auto.driver.findElements(By.xpath("//li[starts-with(@class, 'menu-item')]"));
+            for(WebElement item: menuItems)
+            {
+                System.out.println(item.getText());
+            }
+        // Xpath tương đối 5 and/or
+            // or
+            System.out.println("=================");
+            List<WebElement> subscriptions = auto.driver.findElements(By.xpath("//li[@class ='customer_tab_subscriptions' or @class = 'menu-item-subscriptions']"));
+            for (WebElement item : subscriptions)
+            {
+                System.out.println(item.getText());
+            }
+            // and
+            auto.driver.findElement(By.xpath("//ul[contains(@class, 'customer-tabs') and @role = 'tablist']"));
         Thread.sleep(3000);
         auto.driver.quit();
 
